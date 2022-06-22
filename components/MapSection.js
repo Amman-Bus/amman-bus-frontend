e37765c64bfba77ad70cfe0cf1d16c5bdf717697
BatoolBtoush <batoolbtoush98@gmail.com>
2022-06-22 23:15:59 +0300

    initial steps to building the UI

---
+++ components/MapSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function MapSection () {
+    return (
+        <div></div>
+
+    )
+}
+
+export default MapSection
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import MapSection from '../components/MapSection'
+
 function Home() {
   return (
-    <h1>Hello Bat!</h1>
+    <div>
+      <Head>
+        <title>Amman Bus</title>
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <MapSection />
+
+
+    </div>
+
   )
 }
-
-
 
 export default Home
